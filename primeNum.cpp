55dc53fb979f0aa8ad6bf9792d27ca3c82b5b994
SanjidaAfrinRuet <sinthiafrinbd@gmail.com>
2026-07-28 12:55:28 +0600

    initial Commit: prime number

---
+++ primeNum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int start, end;
+
+    cout << "Enter the starting number: ";
+    cin >> start;
+    cout << "Enter the ending number: ";
+    cin >> end;
+
+    cout << "Prime numbers between " << start << " and " << end << " are:\n";
+
+    bool found = false;
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "No prime numbers found in this range.";
+    }
+    cout << "\n";
+
+    return 0;
+}
